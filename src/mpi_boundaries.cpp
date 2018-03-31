ff4d0d4808a4149c5176dd948e92c88e0093a8ec
Evan Schneider <es26@princeton.edu>
2018-03-31 14:38:54 -0400

    working version of passive scalars and slice output routine with and without MPI

--- src/mpi_boundaries.cpp
+++ src/mpi_boundaries.cpp
@@ -82,8 +82,6 @@ void Grid3D::Set_Boundaries_MPI_BLOCK(int *flags, struct parameters P)
       Wait_and_Unload_MPI_Comm_Buffers_BLOCK(0, flags);
     }
   }
-  printf("%d x boundaries set.\n", procID);
-  fflush(stdout);
   MPI_Barrier(world);
   if (H.ny > 1) {
 
@@ -101,8 +99,6 @@ void Grid3D::Set_Boundaries_MPI_BLOCK(int *flags, struct parameters P)
       Wait_and_Unload_MPI_Comm_Buffers_BLOCK(1, flags);
     }
   }
-  printf("%d y boundaries set.\n", procID);
-  fflush(stdout);
   MPI_Barrier(world);
   if (H.nz > 1) {
 
@@ -110,10 +106,6 @@ void Grid3D::Set_Boundaries_MPI_BLOCK(int *flags, struct parameters P)
     if (flags[4]==5 || flags[5]==5) {
       Load_and_Send_MPI_Comm_Buffers(2, flags);
     }
-
-    printf("%d z boundaries sent.\n", procID);
-    fflush(stdout);
-    MPI_Barrier(world);
 
     /* Step 8 - Set non-MPI z-boundaries */
     Set_Boundaries(4, flags);
@@ -123,14 +115,7 @@ void Grid3D::Set_Boundaries_MPI_BLOCK(int *flags, struct parameters P)
     if (flags[4]==5 || flags[5]==5) {
       Wait_and_Unload_MPI_Comm_Buffers_BLOCK(2, flags);
     }
-
-    printf("%d z boundaries received.\n", procID);
-    fflush(stdout);
-    MPI_Barrier(world);
   }
-  printf("%d z boundaries set.\n", procID);
-  fflush(stdout);
-  MPI_Barrier(world);
 
 }
 
