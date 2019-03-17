49abaaf9b3d798cc1eeb4706b73ad73ba573e13e
bruno <bvillasen@gmail.com>
2019-03-16 19:29:24 -0700

    initialize pfft

--- src/gravity/gravity_functions.cpp
+++ src/gravity/gravity_functions.cpp
@@ -9,13 +9,15 @@ void Grid3D::Initialize_Gravity( struct parameters *P ){
   
   chprintf( "\nInitializing Gravity... \n");
   
+  Grav.Initialize( H.xblocal, H.yblocal, H.zblocal, H.xdglobal, H.ydglobal, H.zdglobal, P->nx, P->ny, P->nz, H.nx_real, H.ny_real, H.nz_real, H.dx, H.dy, H.dz, H.n_ghost_potential_offset  );
+  
 
 
 
 
 
 
-  chprintf( "Gravity Succesfuly Initialized \n\n");
+  chprintf( "Gravity Successfully Initialized \n\n");
 
   
 }
