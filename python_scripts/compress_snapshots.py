9441f2c50192394cfb208c9f4737e987fc7d1a14
bruno <bvillasen@gmail.com>
2019-07-27 16:16:40 -0700

    Updated parameter files

--- python_scripts/compress_snapshots.py
+++ python_scripts/compress_snapshots.py
@@ -8,9 +8,14 @@ import numpy as np
 
 
 
-dataDir = '/gpfs/alpine/proj-shared/ast149/cosmo_tests/'
-inDir = dataDir + 'cosmo_1024/output_snapshots/'
-outDir = dataDir + 'cosmo_1024/snapshots/'
+# dataDir = '/gpfs/alpine/proj-shared/ast149/cosmo_tests/'
+# inDir = dataDir + 'cosmo_1024/output_snapshots/'
+# outDir = dataDir + 'cosmo_1024/snapshots/'
+
+
+dataDir = '/raid/bruno/data/'
+inDir = dataDir + 'cosmo_sims/cholla_pm/256_cool_uv_100Mpc/'
+outDir = inDir + 'data_100/'
 
 hydro = True
 cosmo = True
@@ -44,8 +49,9 @@ print( "Number of files per snapshot: {0}".format(nBoxes) )
 
 
 #Set wich snapshots to compress
-# snapshots_to_compress = snapshots_all
-snapshots_to_compress = [199]
+snapshots_to_compress = snapshots_all
+snapshots_to_compress = [0]
+
 
 print( "\nNumber of snapshots to compres: {0}".format(len(snapshots_to_compress)) )
 
@@ -54,7 +60,7 @@ print( "\nNumber of snapshots to compres: {0}".format(len(snapshots_to_compress)
 #[ density, momentum_x, momentum_y, momentum_z, Enegy, GasEnergy ]
 #[ HI_density, HI_density, HeI_density, HeII_density, HeIII_density, e_density, metal_density, temperature, potential ]
 hydro_fields = 'all'
-hydro_fields = ['density', 'GasEnergy']
+hydro_fields = ['density', 'temperature']
 print( "\nHydro fields: {0}".format(hydro_fields))
 
 #available Particles Fields:
@@ -64,8 +70,8 @@ particles_fields = ['density']
 print( "\nParticles fields: {0}".format(particles_fields))
 
 
-# precision = np.float64
-precision = np.float32
+precision = np.float64
+# precision = np.float32
 # precision = np.float16
 print( "\nPrecision: {0}".format( precision ))
 
