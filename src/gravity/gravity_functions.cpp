50e47a339330775573692bf9bd773c16040a07f3
bruno <bvillasen@gmail.com>
2019-08-30 15:53:49 -0700

    don't initialize cuda when ONLY particles

--- src/gravity/gravity_functions.cpp
+++ src/gravity/gravity_functions.cpp
@@ -173,6 +173,10 @@ void Grid3D::Initialize_Gravity( struct parameters *P ){
   Grav.Initialize( H.xblocal, H.yblocal, H.zblocal, H.xdglobal, H.ydglobal, H.zdglobal, P->nx, P->ny, P->nz, H.nx_real, H.ny_real, H.nz_real, H.dx, H.dy, H.dz, H.n_ghost_potential_offset  );
   chprintf( "Gravity Successfully Initialized. \n\n");
   
+  #ifdef ONLY_PARTICLES
+  chprintf("ONLY_PARTICLES: Initializing without CUDA support.\n");
+  #else
+  
   //initialize cuda for use with mpi
   // Needed to initialize cuda after gravity in order to work on Summit
   #ifdef MPI_CHOLLA
@@ -186,11 +190,8 @@ void Grid3D::Initialize_Gravity( struct parameters *P ){
   }  
   #endif /*CUDA*/
   #endif //MPI_CHOLLA
-  
-  #ifdef ONLY_PARTICLES
-  chprintf("ONLY_PARTICLES: Initializing without CUDA support.\n");
-  #else
   chprintf( "CUDA Successfully Initialized.\n");
+  
   #endif
 }
 
