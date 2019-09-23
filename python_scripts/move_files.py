6ef913fa4cc4ee382f2ce468b7c609a6b10f01bf
bruno <bvillasen@gmail.com>
2019-09-22 18:04:35 -0700

    updated script

--- python_scripts/move_files.py
+++ python_scripts/move_files.py
@@ -5,13 +5,13 @@ import h5py as h5
 dataDir = '/gpfs/alpine/proj-shared/ast149/cosmo_tests/cosmo_1024/'
 snapDir = dataDir + 'output_snapshots/'
 
-nSnap = 1
+nSnap = 150
 
 file_name = 'grid_{0}.h5'.format( nSnap )
 file = h5.File( snapDir + file_name, 'r' )
 current_z = file.attrs['Current_z']
+print( ' Snap: {0}   current_z: {1:.4f}'.format( nSnap, current_z) ) 
 file.close()
 
-print( ' Snap: {0}   current_z: {1:.4f}'.format( nSnap, current_z) ) 
 
 # os.rename('guru99.txt','career.guru99.txt') 
