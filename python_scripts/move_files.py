74f4760568617c9f1f03d76db44451c5460b88ce
bruno <bvillasen@gmail.com>
2019-09-22 18:02:35 -0700

    updated script

--- python_scripts/move_files.py
+++ python_scripts/move_files.py
@@ -9,7 +9,7 @@ nSnap = 1
 
 file_name = 'grid_{0}.h5'.format( nSnap )
 file = h5.File( snapDir + file_name, 'r' )
-current_z = file.attrs['current_z']
+current_z = file.attrs['Current_z']
 file.close()
 
 print( ' Snap: {0}   current_z: {1:.4f}'.format( nSnap, current_z) ) 
