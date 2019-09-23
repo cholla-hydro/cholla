099b6e45c371d69941b456a80c6366be8fcdcc02
bruno <bvillasen@gmail.com>
2019-09-22 17:57:13 -0700

    added script

---
+++ python_scripts/move_files.py
@@ -0,0 +1,16 @@
+import os, sys  
+import h5py as h5
+
+
+dataDir = '/gpfs/alpine/proj-shared/ast149/cosmo_tests/cosmo_1024/'
+snapDir = dataDir + 'output_snapshots/'
+
+nSnap = 0
+
+file_name = 'grid_{0}.h5'.format( nSnap )
+file = h5.File( file_name, 'r' )
+current_z = file.attrs['current_z']
+
+print( ' Snap: {0}   current_z: {1:.4f}'.format( nSnap, current_z) ) 
+
+# os.rename('guru99.txt','career.guru99.txt') 
