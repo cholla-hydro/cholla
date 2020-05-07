6e59b2a304b074c08be101ec0b416f92d830e5e3
Trey White <twhite@cray.com>
2020-05-07 13:27:41 -0500

    Added scripts to use just Paris Poisson solver. Cleaned up NVidia/AMD scripts.

--- nrun-paris.sh
+++ nrun-paris.sh
@@ -4,11 +4,10 @@ module load PrgEnv-cray
 module load hdf5
 module load gcc
 
-OUTDIR="out.cufft.256"
+OUTDIR="out.paris-cuda.$(date +%m%d.%H%M%S)"
 set -x
-rm -rf ${OUTDIR}
 mkdir -p ${OUTDIR}
 cd ${OUTDIR}
 export MV2_USE_CUDA=1
 export MV2_ENABLE_AFFINITY=0
-srun -n1 -c16 -N1 --exclusive -p v100 ../cholla.cufft ../parameter_file.txt |& tee tee
+srun -n4 -c16 -N1 --exclusive -p v100 ../cholla.paris-cuda ../parameter_file.txt |& tee tee
