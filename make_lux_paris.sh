31649530ea2383e36362b872287f724e6aa681c8
Bruno <bvillasen@gmail.com>
2020-06-01 13:44:31 -0700

    updated Makefiles for Lux

--- make_lux_paris.sh
+++ make_lux_paris.sh
@@ -6,6 +6,7 @@ module load cuda10.1/10.1
 module list
 
 export MPI_HOME='/cm/shared/apps/openmpi/openmpi-4.0.1.cuda/'
+export GRAKLE_HOME='/home/brvillas/code/grackle'
 CC	= mpicc
 CXX   = mpic++
 export POISSON_SOLVER='-DPARIS'
