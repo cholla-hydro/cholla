19f87e3a58a561077cb44e3bd7bc70b32cdffa7f
bruno <bvillasen@gmail.com>
2020-06-07 10:06:00 -0700

    added makefiles for SOR and CUFFT poisson solvers

---
+++ make_lux_sor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load hdf5/1.10.6
+module load openmpi/4.0.1-cuda
+module load cuda10.1/10.1
+module list
+
+export MPI_HOME='/cm/shared/apps/openmpi/openmpi-4.0.1.cuda/'
+export GRAKLE_HOME='/home/brvillas/code/grackle'
+CC	= gcc
+CXX   = g++
+export POISSON_SOLVER='-DSOR'
+export SUFFIX='.sor'
+make clean
+make
