dc8faeb6df010a42fe2077a849a08bb163eec063
Bruno <bvillasen@gmail.com>
2020-07-19 20:05:45 -0700

    updated make script

--- nmake_fom_summit.sh
+++ nmake_fom_summit.sh
@@ -4,6 +4,11 @@ module load hdf5
 module load cuda
 module list
 
+#Load ics for scaling test
+mkdir data
+cd data
+wget https://www.dropbox.com/s/ean9331oqacemlq/ics_25Mpc_128_particles.h5?dl=0
+
 export POISSON_SOLVER='-DPARIS'
 export SUFFIX='.paris-cuda'
 make clean
