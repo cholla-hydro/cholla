5ac32b13845bfd57f25be7fd8bad0654eaddc896
Bruno <bvillasen@gmail.com>
2020-07-20 09:38:16 -0700

    updated link for file

--- nmake_fom_summit.sh
+++ nmake_fom_summit.sh
@@ -7,7 +7,7 @@ module list
 #Load ics for scaling test
 mkdir data
 cd data
-wget https://www.dropbox.com/s/ean9331oqacemlq/ics_25Mpc_128.h5
+wget https://www.dropbox.com/s/v5zzuk5ma1a3x6g/ics_25Mpc_128.h5
 wget https://www.dropbox.com/s/ean9331oqacemlq/ics_25Mpc_128_particles.h5
 cd ..
 
