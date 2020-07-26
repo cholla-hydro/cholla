c3711dd96d1018dbf4a7845141c2356fd3c02f07
Bruno <bvillasen@gmail.com>
2020-07-26 16:33:10 -0700

    addes parameter files for scaling tests

--- make_fom_summit.sh
+++ make_fom_summit.sh
@@ -3,14 +3,14 @@
 module load gcc hdf5 cuda
 module list
 
-#Load ics for scaling test
-# mkdir data
-# cd data
-# wget https://www.dropbox.com/s/v5zzuk5ma1a3x6g/ics_25Mpc_128.h5
-# wget https://www.dropbox.com/s/ean9331oqacemlq/ics_25Mpc_128_particles.h5
-# wget https://www.dropbox.com/s/rbtoo3jx9a558ip/ics_25Mpc_256.h5
-# wget https://www.dropbox.com/s/7bq5an37uudtlz0/ics_25Mpc_256_particles.h5
-# cd ..
+# Load ics for scaling test
+mkdir data
+cd data
+wget https://www.dropbox.com/s/v5zzuk5ma1a3x6g/ics_25Mpc_128.h5
+wget https://www.dropbox.com/s/ean9331oqacemlq/ics_25Mpc_128_particles.h5
+wget https://www.dropbox.com/s/rbtoo3jx9a558ip/ics_25Mpc_256.h5
+wget https://www.dropbox.com/s/7bq5an37uudtlz0/ics_25Mpc_256_particles.h5
+cd ..
 
 export POISSON_SOLVER='-DPARIS'
 export SUFFIX='.paris-cuda'
