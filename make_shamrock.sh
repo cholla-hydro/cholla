c08f756ad3c4e46a40ff142f3aa58bd84561e664
bruno <bvillasen@gmail.com>
2020-09-18 14:32:07 -0700

    updated makefile for Grackle single cells tests

--- make_shamrock.sh
+++ make_shamrock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MPI_HOME='/home/bruno/code/openmpi-4.0.4'
-export POISSON_SOLVER='-DSOR'
-export SUFFIX='.sor'
+export POISSON_SOLVER='-DCUFFT'
+export SUFFIX=''
 # make clean
 make
