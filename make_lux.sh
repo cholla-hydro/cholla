708be8f0b94e1d743a5fa343d791cb8cf9cc1cd1
Bruno <bvillasen@gmail.com>
2020-10-01 14:22:58 -0700

    paris again

--- make_lux.sh
+++ make_lux.sh
@@ -6,7 +6,7 @@ module load cuda10.2/10.2
 module list
 
 export MPI_HOME='/cm/shared/apps/openmpi/openmpi-4.0.1.cuda/'
-export POISSON_SOLVER='-DSOR'
+export POISSON_SOLVER='-DPARIS'
 export SUFFIX=''
 make clean
 make
