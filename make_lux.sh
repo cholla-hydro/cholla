b5fdf9fdd434837511cdb2a3542b540cb1bc35a8
Bruno <bvillasen@gmail.com>
2020-10-01 13:36:36 -0700

    now using SOR

--- make_lux.sh
+++ make_lux.sh
@@ -6,7 +6,7 @@ module load cuda10.2/10.2
 module list
 
 export MPI_HOME='/cm/shared/apps/openmpi/openmpi-4.0.1.cuda/'
-export POISSON_SOLVER='-DPARIS'
+export POISSON_SOLVER='-DSOR'
 export SUFFIX=''
 make clean
 make
