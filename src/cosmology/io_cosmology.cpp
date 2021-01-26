adbba80b70037181b95041155093fc56a5eda479
bruno <bvillasen@gmail.com>
2021-01-26 14:57:52 -0800

    chaged scale_index increment when scale_index==0

--- src/cosmology/io_cosmology.cpp
+++ src/cosmology/io_cosmology.cpp
@@ -81,8 +81,9 @@ void Cosmology::Set_Next_Scale_Output(  ){
   Real a_value = scale_outputs[scale_indx];
   chprintf("Setting next output index. Current index: %d    n_outputs: %d ", scale_indx, n_outputs);
   
-  if  ( ( scale_indx == 0 ) && ( abs(a_value - current_a )<1e-5 ) )scale_indx = 1;
-  else scale_indx += 1;
+  // if  ( ( scale_indx == 0 ) && ( abs(a_value - current_a )<1e-5 ) )scale_indx = 1;
+  scale_indx += 1;
+  
   if ( scale_indx < n_outputs ){
     a_value = scale_outputs[scale_indx];
     next_output_indx = scale_indx;
