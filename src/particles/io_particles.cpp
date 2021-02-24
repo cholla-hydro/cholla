6f7b4d3eb39ec6397429ec9ca74990560e2abcf2
bruno <bvillasen@gmail.com>
2021-02-24 15:41:49 -0800

    fixed current_z -> Current_z issue

--- src/particles/io_particles.cpp
+++ src/particles/io_particles.cpp
@@ -94,11 +94,11 @@ void Particles_3D::Load_Particles_Data_HDF5(hid_t file_id, int nfile, struct par
   status = H5Aclose(attribute_id);
 
   #ifdef COSMOLOGY
-  attribute_id = H5Aopen(file_id, "Current_z", H5P_DEFAULT);
+  attribute_id = H5Aopen(file_id, "current_z", H5P_DEFAULT);
   status = H5Aread(attribute_id, H5T_NATIVE_DOUBLE, &current_z);
   status = H5Aclose(attribute_id);
 
-  attribute_id = H5Aopen(file_id, "Current_a", H5P_DEFAULT);
+  attribute_id = H5Aopen(file_id, "current_a", H5P_DEFAULT);
   status = H5Aread(attribute_id, H5T_NATIVE_DOUBLE, &current_a);
   status = H5Aclose(attribute_id);
   #endif
@@ -450,6 +450,16 @@ void Grid3D::Write_Particles_Header_HDF5( hid_t file_id){
   #ifdef SINGLE_PARTICLE_MASS
   attribute_id = H5Acreate(file_id, "particle_mass", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
   status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Particles.particle_mass);
+  status = H5Aclose(attribute_id);
+  #endif
+  
+  #ifdef COSMOLOGY
+  attribute_id = H5Acreate(file_id, "current_z", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Cosmo.current_z);
+  status = H5Aclose(attribute_id);
+  
+  attribute_id = H5Acreate(file_id, "current_a", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Cosmo.current_a);
   status = H5Aclose(attribute_id);
   #endif
 
