66520cd35c47f118c1afbd883ca6f85aecef5cdb
bruno <bvillasen@gmail.com>
2021-02-25 10:34:08 -0800

    updated modules to load for make on summit

--- make_summit.sh
+++ make_summit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-module load gcc
-module load hdf5
-module load cuda
+module loas gcc/6.4.0
+module load hdf5/1.10.4
+module load cuda/10.1.243
+
 
 export MPI_HOME=$(MPI_ROOT)
 export POISSON_SOLVER='-DPARIS'
