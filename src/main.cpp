a17b2441ee71c681c676f5e5c08b0829d14b30b5
Reuben D. Budiardja <reubendb@ornl.gov>
2021-03-03 16:12:33 -0500

    Write before exiting with N_STEPS_LIMIT flag.

--- src/main.cpp
+++ src/main.cpp
@@ -244,7 +244,14 @@ int main(int argc, char *argv[])
     
     #ifdef N_STEPS_LIMIT
     // Exit the loop when reached the limit number of steps (optional)
-    if ( G.H.n_step == N_STEPS_LIMIT) break;
+    if ( G.H.n_step == N_STEPS_LIMIT) {
+      #ifdef GPU_MPI
+      cudaMemcpy(G.C.density, G.C.device, 
+                 G.H.n_fields*G.H.n_cells*sizeof(Real), cudaMemcpyDeviceToHost);
+      #endif
+      WriteData(G, P, nfile);
+      break;
+    }
     #endif
 
     #ifdef COSMOLOGY
