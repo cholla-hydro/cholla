ffa0dd8ac0f202a92f6664889a16b0f21e28fe50
bruno <bvillasen@gmail.com>
2021-04-06 11:51:52 -0700

    updated buidls to use GPU_MPI

--- builds/setup.lux.sh
+++ builds/setup.lux.sh
@@ -4,4 +4,3 @@ module load hdf5/1.10.6 cuda10.2/10.2 openmpi/4.0.1
 
 export MACHINE=lux
 export CHOLLA_ENVSET=1
-export PARIS_MPI_GPU="-DPARIS_NO_GPU_MPI" 
--- builds/setup.shamrock.sh
+++ builds/setup.shamrock.sh
@@ -2,4 +2,4 @@
 
 export MACHINE=shamrock
 export CHOLLA_ENVSET=1
-export PARIS_MPI_GPU="-DPARIS_NO_GPU_MPI" 
+ 
