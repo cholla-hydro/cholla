6828dc1b366bba076f81b15e1dcd1a66d6d76208
bruno <bvillasen@gmail.com>
2021-06-25 14:22:50 -0700

    fixed issue when writing snapshot and analysis file at the same time

--- src/gravity/gravity_functions.cpp
+++ src/gravity/gravity_functions.cpp
@@ -85,8 +85,8 @@ void Grid3D::set_dt_Gravity(){
   #ifdef ANALYSIS
   //Limit delta_a if it's time to run analisys
   if( Analysis.next_output_indx < Analysis.n_outputs ){
-    if ( (Cosmo.current_a + da_min) >  Analysis.next_output ){
-      da_min = Analysis.next_output - Cosmo.current_a;
+    if ( (Cosmo.current_a + da_min) >=  Analysis.next_output ){
+      if ( !H.Output_Now ) da_min = Analysis.next_output - Cosmo.current_a;
       Analysis.Output_Now = true;
     }
   }
