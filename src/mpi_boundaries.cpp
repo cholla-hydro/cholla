1e076380da037137109c76190f1841b2ea957283
Alwin <alwin.mao@pitt.edu>
2021-08-04 05:21:14 -0700

    add ;

--- src/mpi_boundaries.cpp
+++ src/mpi_boundaries.cpp
@@ -576,7 +576,7 @@ void Grid3D::Load_Hydro_DeviceBuffer3D( Real * send_buffer_3d, int axis, int sid
   dim3 dim1dGrid((isize*jsize*ksize+TPB-1)/TPB,1,1);
   dim3 dim1dBlock(TPB, 1, 1);                                                                                              
   
-  hipLaunchKernelGGL(PackBuffers3D,dim1dGrid,dim1dBlock,0,0,send_buffer_3d,c_head,isize,jsize,ksize,H.nx,H.ny,idxoffset,offset,H.n_fields,H.n_cells)
+  hipLaunchKernelGGL(PackBuffers3D,dim1dGrid,dim1dBlock,0,0,send_buffer_3d,c_head,isize,jsize,ksize,H.nx,H.ny,idxoffset,offset,H.n_fields,H.n_cells);
 
     /*
     #pragma omp target teams distribute parallel for collapse ( 3 ) \
