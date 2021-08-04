59cab3b9ce580a711e02d4ea563752570bb25221
Alwin <alwin.mao@pitt.edu>
2021-08-03 21:03:09 -0700

    small test

--- src/mpi_boundaries.cpp
+++ src/mpi_boundaries.cpp
@@ -535,7 +535,7 @@ void Grid3D::Load_Hydro_DeviceBuffer3D( Real * send_buffer_3d, int axis, int sid
   }
 
   int offset = isize*jsize*ksize;
-  int idxoffset = ioffset + joffset*H.nx + koffset*H.nx*H.ny
+  int idxoffset = ioffset + joffset*H.nx + koffset*H.nx*H.ny;
   
     #pragma omp target teams distribute parallel for collapse ( 3 ) \
             private ( idx, gidx ) \
@@ -548,7 +548,7 @@ void Grid3D::Load_Hydro_DeviceBuffer3D( Real * send_buffer_3d, int axis, int sid
         for(k=0;k<ksize;k++)
         {
           idx  = i + (j+k*H.ny)*H.nx + idxoffset;//(i+ioffset) + (j+joffset)*H.nx + (k+koffset)*H.nx*H.ny;
-	  gidx = i+(j+k*jsize)*isize;
+          gidx = i+(j+k*jsize)*isize;
           //gidx = i + j*isize + k*ijsize;//i+(j+k*jsize)*isize
           for (ii=0; ii<H.n_fields; ii++) {
             *(send_buffer_3d + gidx + ii*offset) = c_head[idx + ii*H.n_cells];
