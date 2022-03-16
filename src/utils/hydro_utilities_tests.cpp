fcc745e4d99b18f063095b383c36a087fdca875d
helenarichie <helenarichie@gmail.com>
2022-03-16 17:32:51 -0400

    add test for temperature utility function

--- src/utils/hydro_utilities_tests.cpp
+++ src/utils/hydro_utilities_tests.cpp
@@ -46,6 +46,7 @@ namespace
         std::vector<double> mz {0.6703192739e-100, 0.5676716066e2, 0.2115881803e100};
         std::vector<double> E {20.9342082433e-90, 20.9976906577e10, 20.9487120853e300};
         std::vector<double> P {2.2244082909e-100, 8.6772951021e2, 6.7261085663e100};
+        std::vector<double> n {1.0087201154e-100, 1.0756968986e2, 1.0882403847e100};
         std::vector<std::string> names{"Small number case", "Medium number case", "Large number case"};
     };
 }
