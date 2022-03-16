d3c34b80223c7e9f110990752ce578fe526023bb
helenarichie <helenarichie@gmail.com>
2022-03-16 11:45:34 -0400

    move utility functions from anonymous namespace

--- src/utils/cuda_utilities.h
+++ src/utils/cuda_utilities.h
@@ -14,34 +14,32 @@
 
 
 namespace cuda_utilities {
-    namespace {
-        inline __device__ void Get_GTID(int &id, int &xid, int &yid, int &zid, int &tid, int const &nx, int const &ny, int const &nz) {
-            int blockId = blockIdx.x + blockIdx.y * gridDim.x;
-            int id = threadIdx.x + blockId * blockDim.x;
-            int zid = id / (nx * ny);
-            int yid = (id - zid * nx * ny) / nx;
-            int xid = id - zid * nx * ny - yid * nx;
-            // add a thread id within the block
-            int tid = threadIdx.x;
-        }
-
-        inline __host__ __device__ void Get_Real_Indices(int const &n_ghost, int const &nx, int const &ny, int const &nz, int &is, int &ie, int &js, int &je, int &ks, int &ke) {
-            is = n_ghost;
-            ie = nx - n_ghost;
-            if (ny == 1) {
-                js = 0;
-                je = 1;
-            } else {
-                js = n_ghost;
-                je = ny - n_ghost;
-            }
-            if (nz == 1) {
-                ks = 0;
-                ke = 1;
-            } else {
-                ks = n_ghost;
-                ke = nz - n_ghost;
-            }
-        }   
+    inline __device__ void Get_GTID(int &id, int &xid, int &yid, int &zid, int &tid, int const &nx, int const &ny, int const &nz) {
+        int blockId = blockIdx.x + blockIdx.y * gridDim.x;
+        int id = threadIdx.x + blockId * blockDim.x;
+        int zid = id / (nx * ny);
+        int yid = (id - zid * nx * ny) / nx;
+        int xid = id - zid * nx * ny - yid * nx;
+        // add a thread id within the block
+        int tid = threadIdx.x;
     }
+
+    inline __host__ __device__ void Get_Real_Indices(int const &n_ghost, int const &nx, int const &ny, int const &nz, int &is, int &ie, int &js, int &je, int &ks, int &ke) {
+        is = n_ghost;
+        ie = nx - n_ghost;
+        if (ny == 1) {
+            js = 0;
+            je = 1;
+        } else {
+            js = n_ghost;
+            je = ny - n_ghost;
+        }
+        if (nz == 1) {
+            ks = 0;
+            ke = 1;
+        } else {
+            ks = n_ghost;
+            ke = nz - n_ghost;
+        }
+    }   
 }
--- src/utils/hydro_utilities.h
+++ src/utils/hydro_utilities.h
@@ -28,48 +28,46 @@
 */
 
 namespace hydro_utilities {
-    namespace {
-        inline __host__ __device__ Real Calc_Pressure_Primitive(Real const &E, Real const &d, Real const &vx, Real const &vy, Real const &vz, Real const &gamma) {
-            Real p;
-            p = (E - 0.5 * d * (vx*vx + vy*vy + vz*vz)) * (gamma - 1.0);
-            //std::cout << "\n" << p << "\n";
-            p = fmax(p, TINY_NUMBER);
-            return p;
-        }
-
-        inline __host__ __device__ Real Calc_Pressure_Conserved(Real const &E, Real const &d, Real const &mx, Real const &my, Real const &mz, Real const &gamma) {
-            Real p = (E - 0.5 * (mx*mx + my*my + mz*mz) / d) * (gamma - 1.);
-            return fmax(p, TINY_NUMBER);
-        }
-
-        inline __host__ __device__ Real Calc_Temp(Real const &p, Real const &n) {
-            Real T = p * PRESSURE_UNIT / (n * KB);
-            return T;
-        }
-
-        #ifdef DE
-        inline __host__ __device__ Real _Calc_Temp_DE(Real const &d, Real const &ge, Real const &gamma, Real const&n) {
-            Real T =  d * ge * (gamma - 1.0) * PRESSURE_UNIT / (n * KB);
-            return T;
-        }
-        #endif // DE
-
-        inline __host__ __device__ Real Calc_Energy_Primitive(Real const &p, Real const &d, Real const &vx, Real const &vy, Real const &vz, Real const &gamma) {
-        // Compute and return energy
-        return (fmax(p, TINY_NUMBER)/(gamma - 1.)) + 0.5 * d * (vx*vx + vy*vy + vz*vz);
-        }
-
-        static inline __host__ __device__ Real Get_Pressure_From_DE(Real const &E, Real const &U_total, Real const &U_advected, Real const &gamma){
-            Real U, P;
-            Real eta = DE_ETA_1;
-
-            // Apply same condition as Byan+2013 to select the internal energy from which compute pressure.
-            if( U_total / E > eta ) U = U_total;
-            else U = U_advected;
-
-            P = U * (gamma - 1.0);
-            return P;
-        }
-                                            
+    inline __host__ __device__ Real Calc_Pressure_Primitive(Real const &E, Real const &d, Real const &vx, Real const &vy, Real const &vz, Real const &gamma) {
+        Real p;
+        p = (E - 0.5 * d * (vx*vx + vy*vy + vz*vz)) * (gamma - 1.0);
+        //std::cout << "\n" << p << "\n";
+        p = fmax(p, TINY_NUMBER);
+        return p;
     }
+
+    inline __host__ __device__ Real Calc_Pressure_Conserved(Real const &E, Real const &d, Real const &mx, Real const &my, Real const &mz, Real const &gamma) {
+        Real p = (E - 0.5 * (mx*mx + my*my + mz*mz) / d) * (gamma - 1.);
+        return fmax(p, TINY_NUMBER);
+    }
+
+    inline __host__ __device__ Real Calc_Temp(Real const &p, Real const &n) {
+        Real T = p * PRESSURE_UNIT / (n * KB);
+        return T;
+    }
+
+    #ifdef DE
+    inline __host__ __device__ Real _Calc_Temp_DE(Real const &d, Real const &ge, Real const &gamma, Real const&n) {
+        Real T =  d * ge * (gamma - 1.0) * PRESSURE_UNIT / (n * KB);
+        return T;
+    }
+    #endif // DE
+
+    inline __host__ __device__ Real Calc_Energy_Primitive(Real const &p, Real const &d, Real const &vx, Real const &vy, Real const &vz, Real const &gamma) {
+    // Compute and return energy
+    return (fmax(p, TINY_NUMBER)/(gamma - 1.)) + 0.5 * d * (vx*vx + vy*vy + vz*vz);
+    }
+
+    static inline __host__ __device__ Real Get_Pressure_From_DE(Real const &E, Real const &U_total, Real const &U_advected, Real const &gamma){
+        Real U, P;
+        Real eta = DE_ETA_1;
+
+        // Apply same condition as Byan+2013 to select the internal energy from which compute pressure.
+        if( U_total / E > eta ) U = U_total;
+        else U = U_advected;
+
+        P = U * (gamma - 1.0);
+        return P;
+    }
+
 }
