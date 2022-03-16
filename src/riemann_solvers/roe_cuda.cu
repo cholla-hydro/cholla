7d7597a0627ac60b3427762483e5940ee9ee2692
helenarichie <helenarichie@gmail.com>
2022-03-16 12:49:30 -0400

    add utility function implementation in hydro_cuda.cu

--- src/riemann_solvers/roe_cuda.cu
+++ src/riemann_solvers/roe_cuda.cu
@@ -100,7 +100,7 @@ __global__ void Calculate_Roe_Fluxes_CUDA(Real *dev_bounds_L, Real *dev_bounds_R
     vzl = mzl / dl;
     #ifdef DE //PRESSURE_DE
     E_kin = 0.5 * dl * ( vxl*vxl + vyl*vyl + vzl*vzl );
-    pl = Get_Pressure_From_DE( El, El - E_kin, dgel, gamma );
+    pl = hydro_utilites::Get_Pressure_From_DE( El, El - E_kin, dgel, gamma );
     #else
     pl  = (El - 0.5*dl*(vxl*vxl + vyl*vyl + vzl*vzl)) * (gamma - 1.0);
     #endif //PRESSURE_DE
@@ -118,7 +118,7 @@ __global__ void Calculate_Roe_Fluxes_CUDA(Real *dev_bounds_L, Real *dev_bounds_R
     vzr = mzr / dr;
     #ifdef DE //PRESSURE_DE
     E_kin = 0.5 * dr * ( vxr*vxr + vyr*vyr + vzr*vzr );
-    pr = Get_Pressure_From_DE( Er, Er - E_kin, dger, gamma );
+    pr = hydro_utilities::Get_Pressure_From_DE( Er, Er - E_kin, dger, gamma );
     #else
     pr  = (Er - 0.5*dr*(vxr*vxr + vyr*vyr + vzr*vzr)) * (gamma - 1.0);
     #endif //PRESSURE_DE
