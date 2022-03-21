fe7354da3488f7ce9391d0535902f60cc49efa28
helenarichie <helenarichie@gmail.com>
2022-03-21 12:23:57 -0400

    add test for real index utility function

--- src/utils/cuda_utilities_tests.cpp
+++ src/utils/cuda_utilities_tests.cpp
@@ -44,7 +44,7 @@ namespace
 
 TEST(tHYDROSYSTEMCudaUtilsGetRealIndices, CorrectInputExpectCorrectOutput) {
     TestParams parameters;
-    std::vector<vector<int>> fiducial_indices {{1, 2, 3, 4, 5, 6}, 
+    std::vector<std::vector<int>> fiducial_indices {{1, 2, 3, 4, 5, 6}, 
                                                {4, 5, 6, 7, 8, 9}, 
                                                {7, 8, 9, 4, 5, 6},
                                                {7, 8, 9, 4, 5, 6}}; 
