caa196e3f0f10e0e99857f17cc57f433875b25a8
helenarichie <helenarichie@gmail.com>
2022-03-25 13:46:08 -0400

    add test for real index utility function

--- src/utils/cuda_utilities_tests.cpp
+++ src/utils/cuda_utilities_tests.cpp
@@ -34,7 +34,7 @@ namespace
     struct TestParams
     {
         std::vector<int> n_ghost {2, 2, 3, 4};
-        std::vector<int> nx {1, 2048, 2048, 2048};
+        std::vector<int> nx {100, 2048, 2048, 2048};
         std::vector<int> ny {1, 2048, 2048, 2048};
         std::vector<int> nz {1, 4096, 4096, 4096};
         std::vector<std::string> names {"Single-cell 3D PCM/PLMP case", "Large 3D PCM/PLMP case", "Large PLMC case", "Large PPMP/PPMC case"};
@@ -44,10 +44,10 @@ namespace
 
 TEST(tHYDROSYSTEMCudaUtilsGetRealIndices, CorrectInputExpectCorrectOutput) {
     TestParams parameters;
-    std::vector<std::vector<int>> fiducial_indices {{1, 2, 3, 4, 5, 6}, 
-                                               {4, 5, 6, 7, 8, 9}, 
-                                               {7, 8, 9, 4, 5, 6},
-                                               {7, 8, 9, 4, 5, 6}}; 
+    std::vector<std::vector<int>> fiducial_indices {{2, 98, 0, 1, 0, 1}, 
+                                               {2, 2046, 2, 2046, 2, 2046}, 
+                                               {3, 2045, 3, 2045, 3, 2045},
+                                               {4, 2044, 4, 2044, 4, 2044}}; 
 
     for (size_t i = 0; i < parameters.names.size(); i++)
     {
@@ -59,12 +59,14 @@ TEST(tHYDROSYSTEMCudaUtilsGetRealIndices, CorrectInputExpectCorrectOutput) {
         int ke;
         cuda_utilities::Get_Real_Indices(parameters.n_ghost.at(i), parameters.nx.at(i), parameters.ny.at(i), parameters.nz.at(i), is, ie, js, je, ks, ke);
 
-        std::vector<int> test_indices {is, ie, js, je, ks, ke};
         std::vector<std::string> index_names {"is", "ie", "js", "je", "ks", "ke"};
+        std::vector<int> test_indices {is, ie, js, je, ks, ke};
 
-        for (int j = 0; j < test_indices.size(); j++)
+        std::cout >> test_indices << "\n"
+
+        for (size_t j = 0; j < test_indices.size(); j++)
         {
-            testingUtilities::checkResults(fiducial_indices[i][j], test_indices[i], index_names[j] + " " + parameters.names[i]);
+            testingUtilities::checkResults(fiducial_indices[i][j], test_indices[j], index_names[j] + " " + parameters.names[i]);
         }
     }
 }
