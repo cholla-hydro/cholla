bf6e22f1b1a9550fcd883160fa8b94827e310210
Evan Schneider <eschneider@pitt.edu>
2022-03-29 13:51:03 -0400

    updated MS build

--- src/grid/initial_conditions.cpp
+++ src/grid/initial_conditions.cpp
@@ -1,4 +1,4 @@
- *  \brief Definitions of initial conditions for different tests.
+/*  \brief Definitions of initial conditions for different tests.
            Note that the grid is mapped to 1D as i + (x_dim)*j + (x_dim*y_dim)*k.
            Functions are members of the Grid3D class. */
 
