a48c780afee40aabb7aa0bf3d2de8fde6c937bc1
Evan Schneider <eschneider@pitt.edu>
2022-05-27 12:59:23 -0400

    New gravity regression test.

---
+++ src/system_tests/gravity_system_tests.cpp
@@ -0,0 +1,30 @@
+/*!
+ * \file gravity_system_tests.cpp
+ * \author Evan Schneider (eschneider@pitt.edu)
+ * \brief Contains all the system tests for the GRAVITY build type
+ *
+ */
+
+// External Libraries and Headers
+#include <gtest/gtest.h>
+
+// Local includes
+#include "../system_tests/system_tester.h"
+
+// =============================================================================
+// Test Suite: tGRAVITYSYSTEMSphericalCollapse
+// =============================================================================
+/*!
+ * \defgroup tGRAVITYSYSTEMSphericalCollapse_CorrectInputExpectCorrectOutput
+ * \brief Test spherical collapse with hydro + FFT gravity initial conditions
+ *
+ */
+/// @{
+TEST(tGRAVITYSYSTEMSphericalCollapse,
+     CorrectInputExpectCorrectOutput)
+{
+    systemTest::SystemTestRunner collapseTest;
+    collapseTest.runTest();
+}
+/// @}
+// =============================================================================
