4a92255f98962ac2be1bf7eeeb5111042d096437
Bob Caddy <r.Caddy@pitt.edu>
2023-03-03 16:19:17 -0500

    Remove all support for C in the build system

--- builds/setup.poplar.aomp.sh
+++ builds/setup.poplar.aomp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 module purge
-module load craype-x86-naples craype-network-infiniband 
+module load craype-x86-naples craype-network-infiniband
 module load shared slurm
 module use /home/users/twhite/share/modulefiles
 module load ompi/4.0.4-rocm-3.9 hdf5
 
-export OMPI_CC=$(which clang)
 export OMPI_CXX=$(which clang)
 
 export CHOLLA_MACHINE=poplar.aomp
