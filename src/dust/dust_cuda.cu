3302ae06b27fa100b81f5f8c88b76cbb0ff9bd74
helenarichie <helenarichie@gmail.com>
2023-03-31 16:35:46 -0400

    update comments

--- src/dust/dust_cuda.cu
+++ src/dust/dust_cuda.cu
@@ -1,12 +1,23 @@
+/*!
+ * \file dust_cuda.cu
+ * \author Helena Richie (helenarichie@gmail.com)
+ * \brief Contains code that updates the dust density scalar field. The dust_kernel function determines the rate of 
+ * change of dust density, which is controlled by the sputtering timescale. The sputtering timescale is from the 
+ * McKinnon et al. (2017) model of dust sputtering, which depends on the cell's gas density and temperature.
+ */
+
 #ifdef CUDA
   #ifdef DUST
 
+    // STL includes
     #include <stdio.h>
 
+    // External includes
     #include <cstdio>
     #include <fstream>
     #include <vector>
 
+    // Local includes
     #include "../global/global.h"
     #include "../global/global_cuda.h"
     #include "../grid/grid3D.h"
@@ -28,6 +39,20 @@ void Dust_Update(Real *dev_conserved, int nx, int ny, int nz, int n_ghost, int n
 
 __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_ghost, int n_fields, Real dt, Real gamma)
 {
+  /*!
+  * \brief Compute the change in dust density for a cell and update its value in dev_conserved.
+  *
+  * \param[in] dev_conserved The device conserved variable array
+  * \param[in] nx The number of cells in the x-direction
+  * \param[in] ny The number of cells in the y-direction
+  * \param[in] nz The number of cells in the z-direction
+  * \param[in] n_ghost The number of ghost cells
+  * \param[in] dt 
+  * \param[in] gamma 
+  
+  * \return None
+  */
+
   // get grid indices
   int n_cells = nx * ny * nz;
   int is, ie, js, je, ks, ke;
@@ -38,7 +63,6 @@ __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_g
   int zid     = id / (nx * ny);
   int yid     = (id - zid * nx * ny) / nx;
   int xid     = id - zid * nx * ny - yid * nx;
-  // add a thread id within the block
 
   // define physics variables
   Real d_gas, d_dust;  // fluid mass densities
@@ -52,7 +76,7 @@ __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_g
 
   // define integration variables
   Real dd_dt;          // instantaneous rate of change in dust density
-  Real dd;             // change in dust density at current time-step
+  Real dd;             // change in dust density at current timestep
   Real dd_max = 0.01;  // allowable percentage of dust density increase
   Real dt_sub;         // refined timestep
 
@@ -62,12 +86,14 @@ __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_g
     d_dust = dev_conserved[id + n_cells * grid_enum::dust_density];
     E      = dev_conserved[id + n_cells * grid_enum::Energy];
 
+    // convert mass density to number density
     n = d_gas * DENSITY_UNIT / (mu * MP);
 
     if (E < 0.0 || E != E) {
       return;
     }
 
+    // get conserved quanitites
     vx = dev_conserved[id + n_cells * grid_enum::momentum_x] / d_gas;
     vy = dev_conserved[id + n_cells * grid_enum::momentum_y] / d_gas;
     vz = dev_conserved[id + n_cells * grid_enum::momentum_z] / d_gas;
@@ -86,12 +112,13 @@ __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_g
     T_init = hydro_utilities::Calc_Temp_DE(d_gas, ge, gamma, n);
     #endif  // DE
 
+    // if dual energy is turned on use temp from total internal energy
     T = T_init;
 
-    Real tau_sp = calc_tau_sp(n, T) / TIME_UNIT;  // kyr, sim units
+    Real tau_sp = calc_tau_sp(n, T) / TIME_UNIT;  // sputtering timescale, kyr (sim units)
 
-    dd_dt = calc_dd_dt(d_dust, tau_sp);
-    dd    = dd_dt * dt;
+    dd_dt = calc_dd_dt(d_dust, tau_sp);  // rate of change in dust density at current timestep
+    dd    = dd_dt * dt;  // change in dust density at current timestep
 
     // ensure that dust density is not changing too rapidly
     while (dd / d_dust > dd_max) {
@@ -123,7 +150,7 @@ __device__ __host__ Real calc_tau_sp(Real n, Real T)
   Real omega   = 2.5;
   Real A       = 0.17e9 * YR_IN_S;  // 0.17 Gyr in s
 
-  Real tau_sp = A * (a1 / d0) * (pow(T_0 / T, omega) + 1);  // s
+  Real tau_sp = A * (a1 / d0) * (pow(T_0 / T, omega) + 1);  // sputtering timescale, s
 
   return tau_sp;
 }
