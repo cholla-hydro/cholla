783641ab37da48c5a495699f044d3c5edfd6792d
Bob Caddy <r.Caddy@pitt.edu>
2023-04-14 14:31:19 -0400

    PPMC: Move and update thread guard

--- src/reconstruction/ppmc_cuda_tests.cu
+++ src/reconstruction/ppmc_cuda_tests.cu
@@ -5,16 +5,16 @@
  */
 
 // STL Includes
+#include <algorithm>
 #include <random>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 // External Includes
 #include <gtest/gtest.h>  // Include GoogleTest and related libraries/headers
 
 // Local Includes
-#include <algorithm>
-
 #include "../global/global.h"
 #include "../io/io.h"
 #include "../reconstruction/ppmc_cuda.h"
@@ -30,8 +30,8 @@ TEST(tHYDROPpmcReconstructor, CorrectInputExpectCorrectOutput)
 
   // Mock up needed information
   size_t const nx       = 6;
-  size_t const ny       = 1;
-  size_t const nz       = 1;
+  size_t const ny       = 6;
+  size_t const nz       = 6;
   size_t const n_fields = 5;
   double const dx       = doubleRand(prng);
   double const dt       = doubleRand(prng);
@@ -48,46 +48,63 @@ TEST(tHYDROPpmcReconstructor, CorrectInputExpectCorrectOutput)
   dev_grid.cpyHostToDevice(host_grid);
 
   // Fiducial Data
-  std::unordered_map<int, double> fiducial_interface_left  = {{2, 4.5260179354990537},
-                                                              {8, 0.16067557854687248},
-                                                              {14, 3.7907707014364083},
-                                                              {20, 2.1837489694378442},
-                                                              {26, 3.8877922383184833}};
-  std::unordered_map<int, double> fiducial_interface_right = {{1, 4.5260179354990537},
-                                                              {7, 0.16067557854687248},
-                                                              {13, 3.7907707014364083},
-                                                              {19, 2.1837489694378442},
-                                                              {25, 3.8877922383184833}};
+  std::vector<std::unordered_map<int, double>> fiducial_interface_left = {{{86, 2.6558981128823214},
+                                                                           {302, 0.84399195916314151},
+                                                                           {518, 2.2002498722761787},
+                                                                           {734, 1.764334292986655},
+                                                                           {950, 3.3600925565746804},
+                                                                           {86, 2.4950488327292639}},
+                                                                          {{86, 2.4950488327292639},
+                                                                           {302, 0.79287723513518138},
+                                                                           {518, 1.7614576990062414},
+                                                                           {734, 1.8238574169157304},
+                                                                           {950, 3.14294317122161}},
+                                                                          {{86, 2.6558981128823214},
+                                                                           {302, 0.84399195916314151},
+                                                                           {518, 2.0109603398129137},
+                                                                           {734, 1.764334292986655},
+                                                                           {950, 3.2100231679403066}}};
+
+  std::vector<std::unordered_map<int, double>> fiducial_interface_right = {{{85, 2.6558981128823214},
+                                                                            {301, 0.84399195916314151},
+                                                                            {517, 1.8381070277226794},
+                                                                            {733, 1.764334292986655},
+                                                                            {949, 3.0847691079841209}},
+                                                                           {{80, 3.1281603739188069},
+                                                                            {85, 2.6558981128823214},
+                                                                            {296, 0.99406757727427164},
+                                                                            {301, 0.84399195916314151},
+                                                                            {512, 1.8732124042412865},
+                                                                            {517, 1.8381070277226794},
+                                                                            {728, 1.6489758692176784},
+                                                                            {733, 1.764334292986655},
+                                                                            {944, 2.8820015278590443},
+                                                                            {949, 3.0847691079841209}},
+                                                                           {{50, 2.6558981128823214},
+                                                                            {80, 3.1281603739188069},
+                                                                            {85, 2.6558981128823214},
+                                                                            {266, 0.84399195916314151},
+                                                                            {296, 0.99406757727427164},
+                                                                            {301, 0.84399195916314151},
+                                                                            {482, 2.0109603398129137},
+                                                                            {512, 1.8732124042412865},
+                                                                            {517, 1.8381070277226794},
+                                                                            {698, 1.764334292986655},
+                                                                            {728, 1.6489758692176784},
+                                                                            {733, 1.764334292986655},
+                                                                            {914, 3.2100231679403066},
+                                                                            {944, 2.8820015278590443},
+                                                                            {949, 3.0847691079841209}}};
 
   // Loop over different directions
   for (size_t direction = 0; direction < 3; direction++) {
-    // Assign the shape
-    size_t nx_rot, ny_rot, nz_rot;
-    switch (direction) {
-      case 0:
-        nx_rot = nx;
-        ny_rot = ny;
-        nz_rot = nz;
-        break;
-      case 1:
-        nx_rot = ny;
-        ny_rot = nz;
-        nz_rot = nx;
-        break;
-      case 2:
-        nx_rot = nz;
-        ny_rot = nx;
-        nz_rot = ny;
-        break;
-    }
-
     // Allocate device buffers
     cuda_utilities::DeviceVector<double> dev_interface_left(host_grid.size());
     cuda_utilities::DeviceVector<double> dev_interface_right(host_grid.size());
 
     // Launch kernel
     hipLaunchKernelGGL(PPMC_cuda, dev_grid.size(), 1, 0, 0, dev_grid.data(), dev_interface_left.data(),
-                       dev_interface_right.data(), nx_rot, ny_rot, nz_rot, dx, dt, gamma, direction);
+                       dev_interface_right.data(), nx, ny, nz, dx, dt, gamma, direction);
     CudaCheckError();
     CHECK(cudaDeviceSynchronize());
 
@@ -96,18 +113,20 @@ TEST(tHYDROPpmcReconstructor, CorrectInputExpectCorrectOutput)
       // Check the left interface
       double test_val = dev_interface_left.at(i);
       double fiducial_val =
-          (fiducial_interface_left.find(i) == fiducial_interface_left.end()) ? 0.0 : fiducial_interface_left[i];
+          (fiducial_interface_left.at(direction).find(i) == fiducial_interface_left.at(direction).end())
+              ? 0.0
+              : fiducial_interface_left.at(direction)[i];
 
       testingUtilities::checkResults(
           fiducial_val, test_val,
           "left interface at i=" + std::to_string(i) + ", in direction " + std::to_string(direction));
 
       // Check the right interface
-      test_val = dev_interface_right.at(i);
-      fiducial_val =
-          (fiducial_interface_right.find(i) == fiducial_interface_right.end()) ? 0.0 : fiducial_interface_right[i];
-      // if (test_val != 0)
-      // std::cout << "{" << i << ", " << to_string_exact(test_val) << "}," << std::endl;
+      test_val     = dev_interface_right.at(i);
+      fiducial_val = (fiducial_interface_right.at(direction).find(i) == fiducial_interface_right.at(direction).end())
+                         ? 0.0
+                         : fiducial_interface_right.at(direction)[i];
+
       testingUtilities::checkResults(
           fiducial_val, test_val,
           "right interface at i=" + std::to_string(i) + ", in direction " + std::to_string(direction));
