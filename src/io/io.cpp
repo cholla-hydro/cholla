428da1dd20f3d319645026e22b8354627a72a768
helenarichie <helenarichie@gmail.com>
2023-04-24 14:49:47 -0400

    run clang tidy

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -1434,7 +1434,8 @@ void Grid3D::Write_Grid_HDF5(hid_t file_id)
     #endif  // BASIC_SCALAR
 
     #ifdef DUST
-  Write_Grid_HDF5_Field_GPU(H, file_id, dataset_buffer, device_dataset_buffer, &(C.d_density[H.n_cells*grid_enum::dust_density]), "/dust_density");
+  Write_Grid_HDF5_Field_GPU(H, file_id, dataset_buffer, device_dataset_buffer,
+                            &(C.d_density[H.n_cells * grid_enum::dust_density]), "/dust_density");
     #endif  // DUST
 
     #ifdef OUTPUT_CHEMISTRY
@@ -2387,11 +2388,11 @@ void Grid3D::Read_Grid_HDF5(hid_t file_id, struct parameters P)
 
     #ifdef BASIC_SCALAR
   Read_Grid_HDF5_Field(file_id, dataset_buffer, H, C.scalar, "/scalar0");
-    #endif   // BASIC_SCALAR
+    #endif  // BASIC_SCALAR
 
     #ifdef DUST
-  Read_Grid_HDF5_Field(file_id, dataset_buffer, H, &(C.density[H.n_cells*grid_enum::dust_density]), "/dust_density");
-    #endif   // DUST
+  Read_Grid_HDF5_Field(file_id, dataset_buffer, H, &(C.density[H.n_cells * grid_enum::dust_density]), "/dust_density");
+    #endif  // DUST
 
     #if defined(COOLING_GRACKLE) || defined(CHEMISTRY_GPU)
   Read_Grid_HDF5_Field(file_id, dataset_buffer, H, C.HI_density, "/HI_density");
