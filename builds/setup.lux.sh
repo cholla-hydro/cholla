4ea78a30ad11a5fafeb0ec0d5f7161256c2e1e40
ezlimen <ezlimen@gpu023.cm.cluster>
2023-09-08 11:34:47 -0700

    fix builds

--- builds/setup.lux.sh
+++ builds/setup.lux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-module load hdf5/1.10.6 cuda10.2/10.2 openmpi/4.0.1
+###module load hdf5/1.10.6 cuda10.2/10.2 openmpi/4.0.1
+module load hdf5/1.10.6 cuda11.2 openmpi/4.0.1 devtoolset-9
 
 export MACHINE=lux
 export CHOLLA_ENVSET=1
