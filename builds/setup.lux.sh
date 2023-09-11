d92ee47bdd98f20c3f3e71e765144d0248d19510
ezlimen <ezlimen@gpu010.cm.cluster>
2023-09-11 09:29:26 -0700

    Revert "fix builds"
    This reverts commit 4ea78a30ad11a5fafeb0ec0d5f7161256c2e1e40.

--- builds/setup.lux.sh
+++ builds/setup.lux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-###module load hdf5/1.10.6 cuda10.2/10.2 openmpi/4.0.1
-module load hdf5/1.10.6 cuda11.2 openmpi/4.0.1 devtoolset-9
+module load hdf5/1.10.6 cuda10.2/10.2 openmpi/4.0.1
 
 export MACHINE=lux
 export CHOLLA_ENVSET=1
