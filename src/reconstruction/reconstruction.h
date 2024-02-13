632eae23297bafe0f81a0b579f9249003af30425
Bob Caddy <r.Caddy@pitt.edu>
2024-02-13 14:53:48 -0500

    Move InterfaceState to basic_structs.h
    Moved the struct to avoid circular includes with reconstructor files
    including reconstruction.h which included them.

---
+++ src/reconstruction/reconstruction.h
@@ -0,0 +1,17 @@
+/*!
+ * \file reconstruction.h
+ * \brief Contains the "riemann solved facing" controller function to choose, compute, and return the interface states
+ *
+ */
+
+#ifndef RECONSTRUCTION_H
+#define RECONSTRUCTION_H
+
+#include "../utils/hydro_utilities.h"
+
+namespace reconstruction
+{
+
+}  // namespace reconstruction
+
+#endif  //! RECONSTRUCTION_H
