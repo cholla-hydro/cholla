834c246bb7e877208db6441a1b23ace990139d1d
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-02-29 15:32:51 -0500

    Added support for computing nfw mass-density profile. Also made it possible use potential/mass density profiles for NFW and MN disk on GPUs.

--- src/model/potentials.h
+++ src/model/potentials.h
@@ -15,7 +15,7 @@ struct NFWHaloPotential{
   Real c_vir;  /*!< halo concentration parameter (to account for adiabatic contraction) */
 
   /* function with logarithms used in NFW definitions */
-  static Real log_func(Real y) { return log(1 + y) - y / (1 + y); };
+  __host__ __device__ static Real log_func(Real y) { return log(1 + y) - y / (1 + y); };
 
   /* Cylindrical radial acceleration */
   Real gr_halo_D3D(Real R, Real z) const noexcept
@@ -45,8 +45,24 @@ struct NFWHaloPotential{
     return -C * A * B * z_comp;  // checked with wolfram alpha
   }
 
+  /* Mass density profile */
+  __host__ __device__ Real rho_halo_D3D(Real R, Real z) const noexcept
+  {
+    // by equating eqn 2.67 from Binney and Tremaine with eqn 2 from
+    // Schneider & Robertson (2018) -- these are alternative forms of Phi --
+    // I find that the rho0 normalization is:
+    Real rho0 = M_h / (4 * M_PI * (R_h * R_h * R_h) * log_func(c_vir));
+
+    Real rdivRh = sqrt(R * R + z * z)/R_h;  // spherical radius divided by R_h
+
+    Real rdivRh_p_1 = rdivRh + 1;
+
+    // eqn 2.64 from Binney and Tremmaine:
+    return rho0 / (rdivRh * (rdivRh_p_1 * rdivRh_p_1));
+  }
+
   /* Potential of NFW halo */
-  Real phi_halo_D3D(Real R, Real z) const noexcept
+  __host__ __device__ Real phi_halo_D3D(Real R, Real z) const noexcept
   {
     Real r = sqrt(R * R + z * z);  // spherical radius
     Real x = r / R_h;
@@ -68,8 +84,7 @@ struct NFWHaloPotential{
 struct MiyamotoNagaiDiskProps{
   Real M_d;  /*!< total mass (in Msolar) */
   Real R_d;  /*!< scale-length (in kpc) */
-  Real Z_d;  /*!< scale-height (in kpc). In the case of a gas disk, this is more
-              *!< of an initial guess */
+  Real Z_d;  /*!< scale-height (in kpc). */
 
   /* Radial acceleration in miyamoto nagai */
   Real gr_disk_D3D(Real R, Real z) const noexcept
@@ -94,7 +109,7 @@ struct MiyamotoNagaiDiskProps{
   }
 
    /* Miyamoto-Nagai potential */
-  Real phi_disk_D3D(Real R, Real z) const noexcept
+  __host__ __device__ Real phi_disk_D3D(Real R, Real z) const noexcept
   {
     Real A = sqrt(z * z + Z_d * Z_d);
     Real B = R_d + A;
@@ -104,7 +119,8 @@ struct MiyamotoNagaiDiskProps{
     return -GN * M_d / C;
   };
 
-  Real rho_disk_D3D(const Real r, const Real z) const noexcept
+  /* Mass profile of disk */
+  __host__ __device__ Real rho_disk_D3D(const Real r, const Real z) const noexcept
   {
     const Real a = R_d;
     const Real c = Z_d;
@@ -112,6 +128,15 @@ struct MiyamotoNagaiDiskProps{
     const Real d = a + b;
     const Real s = r * r + d * d;
     return M_d * c * c * (a * (d * d + r * r) + 3.0 * b * d * d) / (4.0 * M_PI * b * b * b * pow(s, 2.5));
+
+    /* version that was ripped out of Potential_Paris_Galactic::Get_Potential
+     *
+     * const Real rho0 = md * zd * zd / (4.0 * M_PI);
+     * const Real a    = sqrt(z * z + Z_d * Z_d);
+     * const Real b    = R_d + a;
+     * const Real c    = r * r + b * b;
+     * return rho0 * (rd * c + 3.0 * a * b * b) / (a * a * a * pow(c, 2.5));
+     */
   }
 };
 
@@ -201,7 +226,7 @@ struct AprroxExponentialDisk3MN{
   }
 
   /* computes the potential */
-  Real phi_disk_D3D(Real R, Real z) const noexcept
+  __host__ __device__ Real phi_disk_D3D(Real R, Real z) const noexcept
   {
     return (comps[0].phi_disk_D3D(R,z) + comps[1].phi_disk_D3D(R,z) +
             comps[2].phi_disk_D3D(R,z));
