a58fad7adadc9227b1928b24819bf5e13a324a73
Brant Robertson <brantr@users.noreply.github.com>
2026-04-27 21:08:29 -0700

    Minor electron density change

--- src/grid/initial_conditions.cpp
+++ src/grid/initial_conditions.cpp
@@ -1898,7 +1898,7 @@ void Grid3D::Iliev0(struct Parameters P)
         C.HeI_density[id]   = rho * 1.0e-10;
         C.HeII_density[id]  = rho * 1.0e-10;
         C.HeIII_density[id] = rho * 1.0e-10;
-        C.e_density[id]     = C.HII_density[id] + C.HeII_density[id]/4.0 + C.HeIII_density[id]/2.0;
+        C.e_density[id]     = C.HII_density[id] + C.HeII_density[id] + 2*C.HeIII_density[id];
       }
     }
   }
