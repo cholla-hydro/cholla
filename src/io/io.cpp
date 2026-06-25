68cbedaf50bd96371d78f1f1598cbc1100fc98f7
Brant Robertson <brantr@users.noreply.github.com>
2026-06-25 13:56:37 -0700

    outdir no longer in Parameters

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -87,20 +87,6 @@ void Write_Data(Grid3D &G, struct Parameters P, int nfile, const io::WriterManag
   cudaMemcpy(G.C.density, G.C.device, G.H.n_fields * G.H.n_cells * sizeof(Real), cudaMemcpyDeviceToHost);
 
   chprintf("\nSaving Snapshot: %d \n", nfile);
-  chprintf("\nOutdir %s\n",P.outdir);
-
-  chprintf("density[0] %e\n",G.C.density[0]);
-  chprintf("Energy[0] %e\n",G.C.Energy[0]);
-  #ifdef DE
-  chprintf("GasEnergy[0] %e\n",G.C.GasEnergy[0]);
-  #endif
-#ifdef CHEMISTRY_GPU
-  chprintf("HI_density[0] %e\n",G.C.HI_density[0]);
-  chprintf("HII_density[0] %e\n",G.C.HII_density[0]);
-  chprintf("HeI_density[0] %e\n",G.C.HeI_density[0]);
-  chprintf("HeII_density[0] %e\n",G.C.HeII_density[0]);
-  chprintf("HeIII_density[0] %e\n",G.C.HeIII_density[0]);
-#endif
 
   // ensure the output-directory exists (try to create it if it doesn't exist)
   Ensure_Dir_Exists(write_manager.fname_template().effective_output_dir_path(nfile));
