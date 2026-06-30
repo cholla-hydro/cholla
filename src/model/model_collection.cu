ecb3a8749684ce58368f8dd6f247ecb8b63f27e1
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-06-29 21:03:58 -0400

    provide a bit of a preview for what is coming in terms of model initialization

--- src/model/model_collection.cu
+++ src/model/model_collection.cu
@@ -8,7 +8,22 @@
 
 ModelCollection::ModelCollection(ParameterMap& pmap)
 {
-  // in the future, we will dynamically build this thing from pmap
+  // as we add new models, we will add more snipets like the following
+  //
+  //   if (pmap.Contains_Table("model.<model-subtable>")) {
+  //     vec_.emplace_back(<ModelClass>(pmap));
+  //   }
+  //
+  // where <ModelClass> will be replaced with the name of the model class and
+  // <model-subtable> will be replaced with the name of the corresponding parameter
+  // file subtable
+
+  // the galaxy_model is still a special case, we will start treating it like a normal
+  // case soon
   ClusteredDiskGalaxy galaxy_model = galaxies::make_MW_model();
   vec_.emplace_back(galaxy_model);
+
+  // once we parse at least one model in the "normal way," we should insert
+  //    pmap.Enforce_Table_Content_Uniform_Access_Status("model", false);
+  // to abort if we haven't read all parameters from within the "model" table
 }
