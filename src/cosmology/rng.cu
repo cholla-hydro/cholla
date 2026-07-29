2661cb085b0329eb10af33579bf90d8a77c4b626
Brant Robertson <brantr@users.noreply.github.com>
2026-07-29 16:33:05 -0700

    Remove documentary comment from src/cosmology/rng.cu
    Co-authored-by: Matthew Abruzzo <matthewabruzzo@gmail.com>

--- src/cosmology/rng.cu
+++ src/cosmology/rng.cu
@@ -70,8 +70,6 @@ __global__ void RNG_Init_GPU(int nx_local, int ny_local, int nz_local, int nx_lo
   }
 }
 
-/*! \fn void RNG_Init_GPU(int nx, int ny, int nz, int n_ghost, unsigned long long seed, unsigned long long subsequence,
- * unsigned long long offset, curandStatePhilox4_32_10_t *state) \brief Initialize a GPU-based RNG */
 __global__ void RNG_Init_TEST(int procID, int nx_local, int ny_local, int nz_local, int nx_local_start,
                               int ny_local_start, int nz_local_start, int nx, int ny, int nz, uint64_t seed,
                               rng_parallel_state_t *states)
