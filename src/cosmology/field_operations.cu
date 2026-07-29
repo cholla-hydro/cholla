e5fef5e40e9d15297d7f153575a9dcaff6b8d369
Brant Robertson <brantr@users.noreply.github.com>
2026-07-29 16:26:36 -0700

    Removing documentary comment from src/cosmology/field_operations.cu
    Co-authored-by: Matthew Abruzzo <matthewabruzzo@gmail.com>

--- src/cosmology/field_operations.cu
+++ src/cosmology/field_operations.cu
@@ -1,8 +1,6 @@
 #include "../utils/gpu.hpp"
 #include "field_operations.h"
 
-/*! \fn void Rescale_Field_GPU(Real *d_x, Real A, int nx, int ny, int nz, int n_ghost)
- *  \brief Multiply one field a multiplicative constant */
 __global__ void Rescale_Field_GPU(Real *d_x, const Real A, int nx, int ny, int nz, int n_ghost)
 {
   // Rescale a field by a multiplicative constant
