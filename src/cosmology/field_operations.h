230669dc2fc6add79629dc4aceceec3bb9f87772
Brant Robertson <brantr@users.noreply.github.com>
2026-07-29 16:25:05 -0700

    Remove vestigial function comment from src/cosmology/field_operations.h
    Co-authored-by: Matthew Abruzzo <matthewabruzzo@gmail.com>

--- src/cosmology/field_operations.h
+++ src/cosmology/field_operations.h
@@ -6,4 +6,4 @@ __global__ void Rescale_Field_GPU(Real *d_x, const Real A, int nx, int ny, int n
 /*! \brief Multiply one field elementwise by another */
 __device__ void Field_Elementwise_Product_GPU(Real *d_x, Real *d_y, int nx, int ny, int nz, int n_ghost);
 // inline __device__ void FFT_Populate_Wavevectors_GPU(Real *d_kx, Real *d_ky, Real *d_kz, Real *d_kk, int n_cells, int
-// n_ghost); inline __device__ void FFT_Field_Reverse_Laplacian(Real *d_x_k, Real *d_kk);
+// n_ghost);
