b9efb1dfd5c931f36a8345122bf27fe1a362aaf3
Brant Robertson <brantr@users.noreply.github.com>
2026-08-21 15:52:12 -0700

    Remove duplicate output directory print statement.

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,6 @@ int main(int argc, char *argv[])
     chprintf("Input directory:  %s\n", P.indir);
   }
   std::strncpy(P.outdir, writer_manager.fname_template().nominal_output_dir_path().c_str(), sizeof(P.outdir)-1);
-  chprintf("Output directory:  %s\n", writer_manager.fname_template().nominal_output_dir_path().c_str());
   chprintf("Output directory:  %s\n", P.outdir);
 
   // Check the configuration
