5b3c6c70ff9bc233772f634b795448f7d797c35d
Brant Robertson <brant@ucsc.edu>
2026-08-21 15:35:34 -0700

    Adding P.outdir back, copying from write_manager.

--- src/global/global.h
+++ src/global/global.h
@@ -274,6 +274,7 @@ struct Parameters {
 #endif /*MPI_CHOLLA*/
   char custom_bcnd[MAXLEN];
   char indir[MAXLEN];  // Folder to load Initial conditions from
+  char outdir[MAXLEN];  // Folder to load Initial conditions from
   Real rho;
   Real vx;
   Real vy;
