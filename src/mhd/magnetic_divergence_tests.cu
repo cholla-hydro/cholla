099d365b6969d835d0a5ac1dd6159123acff7dc4
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-09-18 15:46:40 -0400

    start refactoring tMHDGrid3DcheckMagneticDivergence

--- src/mhd/magnetic_divergence_tests.cu
+++ src/mhd/magnetic_divergence_tests.cu
@@ -57,7 +57,8 @@ TEST(tMHDGrid3DcheckMagneticDivergence, CorrectInputExpectCorrectOutput)
 
   // Perform test
   InitializeChollaMPI(NULL, NULL);
-  double max_magnetic_divergence = mhd::checkMagneticDivergence(G);
+  double max_magnetic_divergence =
+      mhd::checkMagneticDivergence(G.C.device, G.H.dx, G.H.dy, G.H.dz, G.H.nx, G.H.ny, G.H.nz, G.H.n_cells);
   MPI_Finalize();
   // Perform Comparison
   Real const fiducialDivergence = 3.6318132783263106 / 1E15;
